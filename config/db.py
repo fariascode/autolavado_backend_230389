b31165375fba08318b70cd20335d021f33be1c83
Al Farias <farias@MacBook-Air-de-Farias.local>
2026-02-06 00:42:49 -0600

    Actualización del proyecto

---
+++ config/db.py
@@ -0,0 +1,9 @@
+''' Establece 1 conexion con el servidor de base de datos'''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker 
+
+SQLALCHEMY_DATABASE_URL = "mysql://root:230389dev@localhost/autolavado_db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ models/rol.py
@@ -0,0 +1,11 @@
+''' Esta clase permite generar el modelo para los tipos de rol'''
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, Date
+from sqlalchemy.orm import relationship
+from config.db import Base
+
+class Rol(Base):
+    __tablename__ = "tbc_roles"
+    id = Column(Integer, primary_key=True, index=True)
+    nombreRol = Column(String(20))
+    estado = Column(Boolean, default=True)
+    
